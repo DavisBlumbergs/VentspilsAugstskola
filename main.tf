f2acdd391b2accbe5fcc45a4af7f77881c2ded5c
Davis <davisblumb@gmail.com>
2020-04-20 19:31:55 +0300

    Ye

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group_rule" "allow_http" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "0.0.0.0/0"
+  cidr_block = "0.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
@@ -64,7 +64,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public_subnet_eu_central_1b" {
   vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "0.0.0.0/0"
+  cidr_block              = "0.0.0.0/24"
   map_public_ip_on_launch = true
   availability_zone = "eu-central-1b"
   tags = {
