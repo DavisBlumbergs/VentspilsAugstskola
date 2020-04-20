6e7f48e032a6bd34feb94f9ddab41a6ed5311ae0
Davis <davisblumb@gmail.com>
2020-04-20 20:35:56 +0300

    Ye

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_key_pair" "key_pair" {
 resource "aws_instance" "instance" {
   ami = "ami-076431be05aaf8080"
   instance_type = "t2.micro"
-  key_name = "${aws_key_pair.key_pair.key_name}"
+  key_name = "QualificationProject"
 
   provisioner "remote-exec" {
     connection {
