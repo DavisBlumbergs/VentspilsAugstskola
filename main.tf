d265efa67a8066e2fc199cc56b279dbab00dcf0b
crazyguy <52412520+DavisBlumbergs@users.noreply.github.com>
2020-04-21 13:33:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ resource "aws_security_group" "allow_tls" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port = 443
-    to_port = 443
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
