e1f06178d8baa2e0be9a2a8d91bf20a1fbf106ae
crazyguy <52412520+DavisBlumbergs@users.noreply.github.com>
2020-04-21 16:24:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ resource "aws_instance" "instance" {
     }
     inline = [
       "sudo yum install git -y",
-      "sudo amazon-linux-extras install docker -y",
       "sudo service docker start",
+      "sudo amazon-linux-extras install docker -y",
       "sudo usermod -a -G docker ec2-user",
       "docker info"
     ]
