e8a614d66d7046112bfd7eddf6dff156c5fe67a4
crazyguy <52412520+DavisBlumbergs@users.noreply.github.com>
2020-04-22 11:11:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "instance" {
       "sudo amazon-linux-extras install docker -y",
       "sudo usermod -a -G docker ec2-user",
       "docker info",
-      "curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
+      "sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "pwd",
       "cd /home/ec2-user",
