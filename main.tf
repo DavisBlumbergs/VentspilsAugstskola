645b1f6cc33f0085499c688123a7edcbf04f2f66
crazyguy <52412520+DavisBlumbergs@users.noreply.github.com>
2020-06-01 02:22:03 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "instance" {
       "sudo amazon-linux-extras install docker -y",
       "sudo usermod -a -G docker ec2-user",
       "sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
-      "sudo usermod -a -G docker-compose ec2-user",
+      "sudo chmod +x /usr/local/bin/docker-compose",
       "cd /home/ec2-user/",
       "git clone https://DavisBlumbergs:Iddqdidqkfa1@github.com/DavisBlumbergs/VentspilsAugstskola.git",
       "cd VentspilsAugstskola",
