59074223e3ba96a30f88073c26768cd2c8fd4638
crazyguy <52412520+DavisBlumbergs@users.noreply.github.com>
2020-06-01 00:36:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "aws_instance" "instance" {
       "sudo usermod -a -G docker ec2-user",
       "sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo service docker start",
       "pwd",
       "docker info",
       "cd /home/ec2-user/",
